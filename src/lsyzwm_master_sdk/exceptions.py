9e8318bf7bc956a225b276a18c794ec1468f98cb
fhp <fengheping@linkeddt.com>
2025-11-27 20:19:44 +0800

    init code 0.0.1

---
+++ src/lsyzwm_master_sdk/exceptions.py
@@ -0,0 +1,38 @@
+class LsyzwmRpcError(Exception):
+    """RPC 调用异常"""
+
+    def __init__(self, message: str, code: int = -1, data=None):
+        """初始化 RPC 异常
+
+        Args:
+            message: 错误消息
+            code: 错误代码
+            data: 附加数据
+        """
+        super().__init__(message)
+        self.message = message
+        self.code = code
+        self.data = data
+
+    def __str__(self):
+        return f"[Code {self.code}] {self.message}"
+
+
+class LsyzwmZooError(Exception):
+    """ZooKeeper 操作异常"""
+
+    def __init__(self, message: str, code: int = -1, data=None):
+        """初始化 ZooKeeper 异常
+
+        Args:
+            message: 错误消息
+            code: 错误代码
+            data: 附加数据
+        """
+        super().__init__(message)
+        self.message = message
+        self.code = code
+        self.data = data
+
+    def __str__(self):
+        return f"[Code {self.code}] {self.message}"
